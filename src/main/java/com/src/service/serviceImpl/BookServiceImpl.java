823bf407e362362c2a59a6e69a72ca8550f15acf
小麻 <851417245@qq.com>
2020-05-14 12:50:25 +0800

    提交代码

---
+++ src/main/java/com/src/service/serviceImpl/BookServiceImpl.java
@@ -0,0 +1,41 @@
+package com.src.service.serviceImpl;
+
+import com.src.dao.BookMapper;
+import com.src.pojo.Books;
+import com.src.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookServiceImpl implements BookService {
+
+    //servise 层调用dao层
+    @Autowired
+    private BookMapper bookMapper;
+
+    public void setBookMapper(BookMapper bookMapper) {
+        this.bookMapper = bookMapper;
+    }
+
+    public int addBook(Books books) {
+        return bookMapper.addBook(books);
+    }
+
+    public int deleteBookById(int id) {
+        return bookMapper.deleteBookById(id);
+    }
+
+    public int updateBook(Books books) {
+        return bookMapper.updateBook(books);
+    }
+
+    public Books queryBookById(int id) {
+        return bookMapper.queryBookById(id);
+    }
+
+    public List<Books> queryAllBook() {
+        return bookMapper.queryAllBook();
+    }
+}
